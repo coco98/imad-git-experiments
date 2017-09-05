202f136ca3b2ec4d867d5b1100b00295a31ff224
Tanmai Gopal <tanmaig@34cross.in>
2017-09-05 04:59:23 -0700

    new-feature completed!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function aBigNewFeature() {
   console.log('Writing a lot of code.');
   console.log('Halfway done with this new feature now.');
+  console.log('Finally completely done with this new feature.');
 };
 
 function something() {
